4b6d25826bdbaa59c6a8117b62fd2bb851c42362
kongxiangwen <xw.kong@hnair.com>
2018-05-21 13:36:48 +0800

    add spring annotations & add jett server module

--- dubbo-client/src/main/java/ClientMain.java
+++ dubbo-client/src/main/java/ClientMain.java
@@ -1,30 +1,44 @@
 import interfaces.BarService;
 import interfaces.DemoService;
 import interfaces.FooService;
+import models.Book;
+import models.SuperBook;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 /**
  * Created by kongxiangwen on 5/15/18 w:20.
  */
 public class ClientMain {
+
+
 	public static void main(String[] args) {
 		ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext(
 				new String[] { "applicationConsumer.xml" });
 		context.start();
 
-		/*DemoService service = (DemoService) context.getBean("demoService");
+		DemoService service = (DemoService) context.getBean("demoService");
 		System.out.println(service.sayHello("hello"));
 
 
-		FooService foo = (FooService) context.getBean("fooService");
-		System.out.println(foo.sayFoo("world "));*/
+		/*FooService foo = (FooService) context.getBean("fooService");
+		System.out.println(foo.sayFoo("world "));
 
 
 		BarService bar = (BarService) context.getBean("barService");
 		System.out.println(bar.sayBar("world "));
+		*/
+
+		//Book bb = (Book) context.getBean("mybooks");
 
 
+		SuperBook bb = (SuperBook) context.getBean("superBook");
+		System.out.println("book infor from spring:"+bb.bk.getAuthor());
+		/* wrong
+		SuperBook bbb = new SuperBook();
+		System.out.println(bbb.bk.getAuthor());
 		System.out.println("dddd");
+		*/
 		context.close();
 	}
 }
---
+++ dubbo-client/src/main/java/models/SuperBook.java
@@ -0,0 +1,15 @@
+package models;
+
+import javax.annotation.Resource;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+/**
+ * Created by kongxiangwen on 5/18/18 w:20.
+ */
+@Component
+public class SuperBook {
+	//@Autowired
+	@Resource(name="book")
+	public Book bk;
+}
